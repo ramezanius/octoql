32140c8d917e3e5d9b85da5417f6444580d57e35
Sadegh Ramezani <me@ramezanius.me>
2020-08-07 11:27:16 +0430

    [feat] Improve command-line utility
    * Add global flags
    * Add alias for commands

--- octoql.go
+++ octoql.go
@@ -7,27 +7,46 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/ramezanius/octoql/cmd"
+	"github.com/ramezanius/octoql/pkg/config"
 )
 
 func main() {
 	app := &cli.App{
-		Name:  "OctoQL",
+		Name: "OctoQL",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "env",
+				Value:   "DEV",
+				Usage:   "set environment",
+				EnvVars: []string{"ENV"},
+				Aliases: []string{"e"},
+			},
+			&cli.StringFlag{
+				Name:    "cfg",
+				Value:   config.DefaultPath,
+				Usage:   "set configuration path",
+				Aliases: []string{"c"},
+			},
+		},
 		Usage: "OctoQL’s command-line utility for administrative tasks",
 		Commands: []*cli.Command{
 			{
-				Name:   "runserver",
-				Action: cmd.RunServer,
-				Usage:  "Starts a Web server on the machine",
+				Name:    "runserver",
+				Action:  cmd.RunServer,
+				Aliases: []string{"r"},
+				Usage:   "Starts a Web server on the machine",
 			},
 			{
-				Name:   "generate",
-				Action: cmd.Generate,
-				Usage:  "Generates new code based on the changes detected to your schema",
+				Name:    "generate",
+				Action:  cmd.Generate,
+				Aliases: []string{"g"},
+				Usage:   "Generates new code based on the changes detected to your schema",
 			},
 			{
-				Name:   "migrate",
-				Action: cmd.Migrate,
-				Usage:  "Synchronizes the database state with the current set of models and migrations",
+				Name:    "migrate",
+				Action:  cmd.Migrate,
+				Aliases: []string{"m"},
+				Usage:   "Synchronizes the database state with the current set of models and migrations",
 			},
 		},
 	}
